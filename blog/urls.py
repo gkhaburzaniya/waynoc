611c1768238e12244e5b7ea7aeeec713c603c5ec
George Khaburzaniya <dobleman@gmail.com>
2017-09-25 19:27:10 -0700

    Add Form for creating new blog entries.
    * Order blog entries by reverse date.
    * Put some styling in

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from .models import Post
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(model=Post), name='post_list'),
-    url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Post), name='post_detail')
+    url(r'^$', ListView.as_view(model=Post, ordering='-published_date'), name='post_list'),
+    url(r'^add/$', CreateView.as_view(model=Post,
+                                      fields=['title', 'text'],
+                                      success_url=reverse_lazy('post_list')), name='post_form'),
 ]
