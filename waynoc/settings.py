2e9451326295919a85846dfc654ddb699fc5465c
George Khaburzaniya <dobleman@gmail.com>
2017-09-27 18:49:16 -0700

    Make mobile friendly * Add favicon * Move staticfiles and css to globals

--- waynoc/settings.py
+++ waynoc/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'waynoc.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
