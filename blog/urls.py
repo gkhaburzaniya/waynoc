49110fcbc8bba958ca8ef6d3ebf172b1d0a22a3d
George Khaburzaniya <george.khaburzaniya@gmail.com>
2017-10-09 20:45:44 -0700

    Handle url hacking via permission_required.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.conf.urls import url
 from django.views.generic import ListView, CreateView
+from django.contrib.auth.decorators import permission_required
 
 from .models import Post
 
@@ -8,7 +9,8 @@ app_name = 'blog'
 urlpatterns = [
     url(r'^$', ListView.as_view(model=Post, ordering='-date'),
         name='post_list'),
-    url(r'^add/$', CreateView.as_view(model=Post, fields=['title', 'text'],
-                                      success_url=reverse_lazy('blog:post_list')),
+    url(r'^add/$', permission_required('blog.add_post', raise_exception=True)(
+            CreateView.as_view(model=Post, fields=['title', 'text'],
+                               success_url=reverse_lazy('blog:post_list'))),
         name='post_add'),
 ]
--- movienite/urls.py
+++ movienite/urls.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.conf.urls import url
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.decorators import permission_required
 
 from .models import Person, Movie
 from .views import CreateMovieView
@@ -13,9 +14,11 @@ urlpatterns = [
         name='person_detail'),
     url(r'^movie_list/$', ListView.as_view(model=Movie, ordering='-date'),
         name='movie_list'),
-    url(r'^movie_add/$', CreateMovieView.as_view(success_url=reverse_lazy('movienite:movie_list')),
+    url(r'^movie_add/$', permission_required('movienite.add_movie', raise_exception=True)(
+            CreateMovieView.as_view(success_url=reverse_lazy('movienite:movie_list'))),
         name='movie_add'),
-    url(r'^person_add/$', CreateView.as_view(model=Person, fields=['name'],
-                                             success_url=reverse_lazy('movienite:person_list')),
+    url(r'^person_add/$', permission_required('movienite.add_person', raise_exception=True)(
+            CreateView.as_view(model=Person, fields=['name'],
+                               success_url=reverse_lazy('movienite:person_list'))),
         name='person_add'),
 ]
