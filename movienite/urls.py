d18ff14959210fbb1d30094db2a76e9368cd1366
George Khaburzaniya <george.khaburzaniya@gmail.com>
2017-10-17 19:07:15 -0700

    Speed up loading of movie_list using prefetch_related.

--- movienite/urls.py
+++ movienite/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
 
 from .models import Person, Movie
-from .views import CreateMovieView
+from .views import MovieCreateView
 
 app_name = 'movienite'
 urlpatterns = [
@@ -11,8 +11,11 @@ urlpatterns = [
         name='person_list'),
     url(r'^person_detail/(?P<pk>[0-9]+)/$', DetailView.as_view(model=Person),
         name='person_detail'),
-    url(r'^movie_list/$', ListView.as_view(model=Movie),
+    url(r'^movie_list/$', ListView.as_view(
+            model=Movie,
+            queryset=Movie.objects.prefetch_related('attendees')
+                                  .select_related('picker')),
         name='movie_list'),
-    url(r'^movie_add/$', login_required(CreateMovieView.as_view()),
+    url(r'^movie_add/$', login_required(MovieCreateView.as_view()),
         name='movie_add'),
 ]
--- movienite/views.py
+++ movienite/views.py
@@ -1,18 +1,23 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
 from .models import Person, Movie
 from .forms import MovieForm
 
 
-class CreateMovieView(CreateView):
+class MovieCreateView(CreateView):
     model = Movie
     form_class = MovieForm
     success_url = reverse_lazy('movienite:movie_list')
 
     def form_valid(self, form, *args, **kwargs):
         form_data = form.cleaned_data
-        for name in (x for x in form_data['other_attendees'].split(',') if x):
+        cleaned_names = (
+            x.strip()
+            for x
+            in form_data['other_attendees'].split(',')
+            if x
+        )
+        for name in cleaned_names:
             attendee = Person.objects.create(name=name.strip())
             form_data['attendees'] |= Person.objects.filter(id=attendee.id)
         response = super().form_valid(form, *args, **kwargs)
