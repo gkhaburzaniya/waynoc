662de2e60377623ddb3ff893ac8636830f266c93
George Khaburzaniya <george.khaburzaniya@gmail.com>
2017-11-08 17:37:07 -0800

    Use permissions for blog * Add ability to edit and delete blog posts without admin.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.conf.urls import url
-from django.views.generic import ListView, CreateView
-from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import permission_required
 
 from .models import Post
 
@@ -14,11 +14,27 @@ urlpatterns = [
     ),
     url(
         r'^add/$',
-        login_required(CreateView.as_view(
+        permission_required('blog.add_post')(CreateView.as_view(
             model=Post,
-            fields=['title', 'text'],
+            fields=['title', 'date', 'text'],
             success_url=reverse_lazy('blog:post_list')
         )),
         name='post_add'
     ),
+    url(
+        r'^post_edit/(?P<pk>[0-9]+)/$',
+        permission_required('blog.change_post')(UpdateView.as_view(
+            model=Post,
+            fields=['title', 'date', 'text'])
+        ),
+        name='post_edit'
+    ),
+    url(
+        r'^movie_delete/(?P<pk>[0-9]+)/$',
+        permission_required('blog.delete_post')(DeleteView.as_view(
+            model=Post,
+            success_url=reverse_lazy('blog:post_list')
+        )),
+        name='post_delete'
+    )
 ]
