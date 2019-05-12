a027cb156267009fdf7181a0ad930d2911edc1e7
George Khaburzaniya <george.khaburzaniya@gmail.com>
2019-05-12 11:22:54 -0700

    Move views to their own module.

--- movienite/urls.py
+++ movienite/urls.py
@@ -1,52 +1,15 @@
-from django.urls import reverse_lazy, path
-from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
-from django.contrib.auth.decorators import login_required
+from django.urls import path
 
-from .forms import MovieForm
-from .models import Person, Movie
+from .views import (PersonList, PersonDetail, PersonEdit, MovieList, MovieAdd,
+                    MovieEdit, MovieDelete)
 
 app_name = 'movienite'
 urlpatterns = [
-    path(
-        '',
-        ListView.as_view(model=Person),
-        name='person_list'
-    ),
-    path(
-        'person_detail/<int:pk>/',
-        DetailView.as_view(model=Person),
-        name='person_detail'
-    ),
-    path(
-        'person_edit/<int:pk>/',
-        login_required(UpdateView.as_view(model=Person, fields=['name'])),
-        name='person_edit'
-    ),
-    path(
-        'movie_list/',
-        ListView.as_view(
-            model=Movie,
-            queryset=Movie.objects.prefetch_related('attendees').select_related('picker')),
-        name='movie_list'
-    ),
-    path(
-        'movie_add/',
-        login_required(CreateView.as_view(model=Movie,
-                                          form_class=MovieForm,
-                                          success_url=reverse_lazy('movienite:movie_list'))),
-        name='movie_add'
-    ),
-    path(
-        'movie_edit/<int:pk>/',
-        login_required(UpdateView.as_view(model=Movie,
-                                          form_class=MovieForm,
-                                          success_url=reverse_lazy('movienite:movie_list'))),
-        name='movie_edit'
-    ),
-    path(
-        'movie_delete/<int:pk>',
-        login_required(DeleteView.as_view(model=Movie,
-                                          success_url=reverse_lazy('movienite:movie_list'))),
-        name='movie_delete'
-    )
+    path('', PersonList, name='person_list'),
+    path('person_detail/<int:pk>/', PersonDetail, name='person_detail'),
+    path('person_edit/<int:pk>/', PersonEdit, name='person_edit'),
+    path('movie_list/', MovieList, name='movie_list'),
+    path('movie_add/', MovieAdd, name='movie_add'),
+    path('movie_edit/<int:pk>/', MovieEdit, name='movie_edit'),
+    path('movie_delete/<int:pk>', MovieDelete, name='movie_delete')
 ]
---
+++ movienite/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.decorators import permission_required
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView, DetailView, ListView, UpdateView, DeleteView)
+
+from .forms import MovieForm
+from .models import Person, Movie
+
+PersonList = ListView.as_view(model=Person)
+PersonDetail = DetailView.as_view(model=Person)
+PersonEdit = permission_required('movienite.change_person')(
+    UpdateView.as_view(model=Person, fields=['name']))
+
+MovieList = ListView.as_view(
+    model=Movie,
+    queryset=Movie.objects.prefetch_related(
+        'attendees').select_related('picker'))
+MovieAdd = permission_required('movienite.add_movie')(
+    CreateView.as_view(model=Movie, form_class=MovieForm,
+                       success_url=reverse_lazy('movienite:movie_list')))
+MovieEdit = permission_required('movienite.change_movie')(
+    UpdateView.as_view(model=Movie, form_class=MovieForm,
+                       success_url=reverse_lazy('movienite:movie_list')))
+MovieDelete = permission_required('movienite.delete_movie')(
+    DeleteView.as_view(model=Movie,
+                       success_url=reverse_lazy('movienite:movie_list')))
