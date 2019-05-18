15041bc56b5a903ac4ffc3f4998f292c581dcf5a
George Khaburzaniya <george.khaburzaniya@gmail.com>
2019-05-18 14:22:18 -0700

    Improve backups.

--- db_backup.py
+++ db_backup.py
@@ -14,5 +14,12 @@ today = date.today()
 copyfile(DATABASES['default']['NAME'], os.path.join(BACKUP_DIR, str(today)))
 
 for backup in os.listdir(BACKUP_DIR):
-    if today - date.fromisoformat(backup) > timedelta(days=30):
+    backup_date = date.fromisoformat(backup)
+
+    yearly = backup_date.month == 1 and backup_date.day == 1
+    monthly = backup_date.day == 1 and backup_date.year == today.year
+    weekly = (backup_date.isoweekday() == 1
+              and today - backup_date < timedelta(days=30))
+    daily = today - backup_date < timedelta(days=7)
+    if not yearly and not monthly and not weekly and not daily:
         os.remove(os.path.join(BACKUP_DIR, backup))
