01ea36efad5c4e8e169e9fc07c0819c9fbb42a74
George Khaburzaniya <george.khaburzaniya@gmail.com>
2019-06-08 13:34:09 -0700

    Still need staticfiles for dev.

--- waynoc/settings.py
+++ waynoc/settings.py
@@ -107,6 +107,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
