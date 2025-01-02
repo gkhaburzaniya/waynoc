a7dcd64597868a513b690093f722c2711a2ed93f
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-02 09:26:16 -0800

    Add a square.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -1,3 +1,8 @@
-from pyscript.web import page
+from pyscript.web import page, div
 
 page["#loading"][0].remove()
+main = page["main"][0]
+
+field = div(style={"width": "300px", "height": "300px", "border": "2px solid black"})
+
+main.append(field)
