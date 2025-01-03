0f44c844744c5540589a4fef1d7d8e2e858fd1e9
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-03 10:32:30 -0800

    Add a player box

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -3,6 +3,12 @@ from pyscript.web import page, div
 page["#loading"][0].remove()
 main = page["main"][0]
 
-field = div(style={"width": "300px", "height": "300px", "border": "2px solid black"})
+field = div(style={"width": "300px", "height": "300px",
+                   "background-color": "grey",
+                   "border": "2px solid black"})
+player = div(style={"width": "50px", "height": "50px",
+                    "background-color": "blue",
+                    "border": "2px solid black"})
 
+field.append(player)
 main.append(field)
