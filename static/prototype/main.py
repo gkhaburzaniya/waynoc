75f80dacbff78f3473203a86d5d64a62b3d08b23
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-03 11:20:25 -0800

    Add moveup ability to player.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -1,4 +1,5 @@
 from pyscript.web import page, div
+from pyscript import when, window
 
 page["#loading"][0].remove()
 main = page["main"][0]
@@ -10,7 +11,7 @@ field = div(style={"width": "300px", "height": "300px",
                    "border": "2px solid black"})
 player = div(style={"width": "50px", "height": "50px",
                     "position": "absolute",
-                    "bottom": "0px",
+                    "bottom": "0%",
                     "background-color": "blue",
                     "border": "2px solid black"})
 enemy = div(style={"width": "50px", "height": "50px",
@@ -20,3 +21,9 @@ enemy = div(style={"width": "50px", "height": "50px",
 field.append(enemy)
 field.append(player)
 main.append(field)
+
+
+@when("keydown", window)
+def move(event):
+    if event.key == "ArrowUp":
+        player.style["bottom"] = str(int(player.style["bottom"][:-1]) + 5) + "%"
