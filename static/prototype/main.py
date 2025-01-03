5b1df8b9db027c7afd97c8efced641ab814d2eff
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-03 11:25:10 -0800

    Player class to make move easier.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -4,26 +4,39 @@ from pyscript import when, window
 page["#loading"][0].remove()
 main = page["main"][0]
 
+
+class Player:
+    def __init__(self):
+        self.element = div(style={"width": "50px", "height": "50px",
+                                  "position": "absolute",
+                                  "bottom": "0%",
+                                  "background-color": "blue",
+                                  "border": "2px solid black"})
+        self.x = 0
+        self.y = 0
+
+    def move(self, x=0, y=0):
+        self.x += x
+        self.y += y
+        self.element.style["bottom"] = str(self.x) + "%"
+        self.element.style["left"] = str(self.y) + "%"
+
+
 field = div(style={"width": "300px", "height": "300px",
                    "display": "flex",
                    "position": "relative",
                    "background-color": "grey",
                    "border": "2px solid black"})
-player = div(style={"width": "50px", "height": "50px",
-                    "position": "absolute",
-                    "bottom": "0%",
-                    "background-color": "blue",
-                    "border": "2px solid black"})
 enemy = div(style={"width": "50px", "height": "50px",
                    "background-color": "red",
                    "border": "2px solid black"})
-
+player = Player()
 field.append(enemy)
-field.append(player)
+field.append(player.element)
 main.append(field)
 
 
 @when("keydown", window)
 def move(event):
     if event.key == "ArrowUp":
-        player.style["bottom"] = str(int(player.style["bottom"][:-1]) + 5) + "%"
+        player.move(x=5)
