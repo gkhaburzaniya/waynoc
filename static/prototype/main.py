504ef529da001f0f4e4c8598d4abacfd7272ef1c
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-03 10:39:26 -0800

    Move player to bottom.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -4,11 +4,19 @@ page["#loading"][0].remove()
 main = page["main"][0]
 
 field = div(style={"width": "300px", "height": "300px",
+                   "display": "flex",
+                   "position": "relative",
                    "background-color": "grey",
                    "border": "2px solid black"})
 player = div(style={"width": "50px", "height": "50px",
+                    "position": "absolute",
+                    "bottom": "0px",
                     "background-color": "blue",
                     "border": "2px solid black"})
+enemy = div(style={"width": "50px", "height": "50px",
+                   "background-color": "red",
+                   "border": "2px solid black"})
 
+field.append(enemy)
 field.append(player)
 main.append(field)
