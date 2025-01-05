6eab23af4937993dce7d709b2fb310e5f91f68d7
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-05 07:10:21 -0800

    Make x a property.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -12,7 +12,6 @@ class Player:
     moving_up = False
     moving_down = False
     moving = False
-    x = 0
     y = 0
 
     def __init__(self):
@@ -21,12 +20,21 @@ class Player:
                 "width": "5%",
                 "height": "5%",
                 "position": "absolute",
+                "left": "0%",
                 "bottom": "0%",
                 "background-color": "blue",
                 "border": "2px solid black",
                 "transition": "0.05s linear",
             }
         )
+
+    @property
+    def x(self):
+        return int(self.element.style["left"][:-1])
+
+    @x.setter
+    def x(self, value):
+        self.element.style["left"] = str(value) + "%"
 
     async def move(self):
         move_speed = 2
@@ -35,11 +43,9 @@ class Player:
             self.moving_left or self.moving_right or self.moving_up or self.moving_down
         ):
             if self.moving_right and self.x != 100:
-                self.x += move_speed
-                self.element.style["left"] = str(self.x) + "%"
+                self.x = self.x + move_speed
             elif self.moving_left and self.x != 0:
-                self.x -= move_speed
-                self.element.style["left"] = str(self.x) + "%"
+                self.x = self.x - move_speed
             if self.moving_up and self.y != 100:
                 self.y += move_speed
                 self.element.style["bottom"] = str(self.y) + "%"
@@ -74,7 +80,6 @@ field.append(player.element)
 main.append(field)
 
 
-# TODO make movement bound by field
 @when("keydown", window)
 def keydown(event):
     if event.key == "ArrowUp":
