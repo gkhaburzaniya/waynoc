c5274ab9040f47cbf6da723d335920187a5e5b82
George Khaburzaniya <george.khaburzaniya@gmail.com>
2025-01-16 09:33:53 -0800

    Use a more-equal-to-frame sleep.

--- static/prototype/main.py
+++ static/prototype/main.py
@@ -59,7 +59,7 @@ class Player:
         move_speed = 5
         self.moving = True
         while (
-                self.moving_left or self.moving_right or self.moving_up or self.moving_down
+            self.moving_left or self.moving_right or self.moving_up or self.moving_down
         ):
             if self.moving_right and self.x != (295 - self.width):
                 self.x += move_speed
@@ -89,19 +89,19 @@ def check_collision(mana_blast):
     mana_blast_rect = mana_blast.getBoundingClientRect()
     enemy_rect = enemy.getBoundingClientRect()
     if (
-            mana_blast_rect.top < enemy_rect.bottom and
-            mana_blast_rect.bottom > enemy_rect.top and
-            mana_blast_rect.left < enemy_rect.right and
-            mana_blast_rect.right > enemy_rect.left
+        mana_blast_rect.top < enemy_rect.bottom
+        and mana_blast_rect.bottom > enemy_rect.top
+        and mana_blast_rect.left < enemy_rect.right
+        and mana_blast_rect.right > enemy_rect.left
     ):
         enemy.remove()
         mana_blast.remove()
 
 
 async def blast_finish(mana_blast):
-    for _ in range(300):
+    for _ in range(60):
         check_collision(mana_blast)
-        await asyncio.sleep(0.01)
+        await asyncio.sleep(0.05)
         # main.append(div(window.requestAnimationFrame(check_collision)))
 
 
